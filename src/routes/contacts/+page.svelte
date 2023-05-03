a41d341c7ecd201a2d64f4c9d4921c5b27edcfbe
vighub <gvigan97@gmail.com>
2023-05-03 23:21:49 +0200

    added contact form

--- src/routes/contacts/+page.svelte
+++ src/routes/contacts/+page.svelte
@@ -1,7 +1,69 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	import { form, field } from 'svelte-forms';
+	import { required, email, min } from 'svelte-forms/validators';
+	import { Toast, toastStore } from '@skeletonlabs/skeleton';
+	import type { ToastSettings } from '@skeletonlabs/skeleton';
+
+	const mail = field('mail', '', [min(1), required(), email()]);
+	const subject = field('subject', '', [required()]);
+	const emailContent = field('emailContent', '', [required()]);
+	let clicked = 0;
+
+	const emailForm = form(mail, subject, emailContent);
+
+	const toastSettings: ToastSettings = {
+		message: 'Email sent successfully!',
+		timeout: 7000,
+	};
+
+	const handleSubmit = () => {
+		clicked += 1;
+		emailForm.validate();
+		if ($emailForm.dirty && $emailForm.valid) {
+			clicked = 0;
+			emailForm.reset();
+			toastStore.trigger(toastSettings);
+		}
+	};
+</script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1>CONTACTS</h1>
+	<div class="w-[80%]">
+		<form>
+			<h2 class="ms-3 mb-7">Send me an email!</h2>
+			<label class="label m-3">
+				<span>From</span>
+				<input class="input" type="text" placeholder="Your email" bind:value={$mail.value} />
+				{#if $emailForm.hasError('mail.required') && clicked > 0}
+					<div class="dark:text-error-400 text-error-500">Your email is required</div>
+				{/if}
+				{#if $emailForm.hasError('mail.not_an_email') && clicked > 0}
+					<div class="dark:text-error-400 text-error-500">Write a valid email</div>
+				{/if}
+			</label>
+			<label class="label m-3">
+				<span>Subject</span>
+				<input class="input" type="text" placeholder="Subject" bind:value={$subject.value} />
+				{#if $emailForm.hasError('subject.required') && clicked > 0}
+					<div class="dark:text-error-400 text-error-500">Subject of your email is required</div>
+				{/if}
+			</label>
+			<label class="label m-3">
+				<span>Text</span>
+				<textarea
+					class="textarea rounded-2xl"
+					rows="4"
+					placeholder="Tell me what you want!"
+					bind:value={$emailContent.value}
+				/>
+				{#if $emailForm.hasError('emailContent.required') && clicked > 0}
+					<div class="dark:text-error-400 text-error-500">Don't you want to write me anything?</div>
+				{/if}
+			</label>
+			<button disabled={!$emailForm.valid} on:click={handleSubmit} class="btn variant-ghost-primary relative left-[90%]"
+				>Send</button
+			>
+		</form>
+		<Toast />
 	</div>
 </div>
