ff2bdef6f5f9ded8216246fe05e6d656a726d37d
vighub <gvigan97@gmail.com>
2023-05-05 01:42:31 +0200

    refactored and improved mobile navbar

--- src/lib/structure/LeftNavbar.svelte
+++ src/lib/structure/LeftNavbar.svelte
@@ -1,16 +1,31 @@
-<script>
+<script lang="ts">
+	import { page } from '$app/stores';
 	import { drawerStore } from '@skeletonlabs/skeleton';
 
 	const drawerClose = () => {
 		drawerStore.close();
 	};
+	const styleClass = 'ms-1 mt-2  p-3 rounded-3xl hover:bg-[#f1f5f9] dark:hover:bg-[#334155]';
+	const aStyle = '!no-underline !text-black dark:!text-white';
+	$: classesActive = (href: string) =>
+		href === $page.url.pathname
+			? 'bg-[#f1f5f9] dark:bg-[#334155] border-sky-700 dark:border-emerald-500 border-x-2'
+			: '';
 </script>
 
-<nav class="list-nav p-4">
-	<ul>
-		<li><a href="/" on:click={drawerClose}>Home</a></li>
-		<li><a href="/about" on:click={drawerClose}>About</a></li>
-		<li><a href="/projects" on:click={drawerClose}>Projects</a></li>
-		<li><a href="/contacts" on:click={drawerClose}>Contact Me!</a></li>
+<nav class="p-4">
+	<ul class="">
+		<a href="/" on:click={drawerClose} class={aStyle}>
+			<li class="{classesActive('/')} {styleClass}">Home</li>
+		</a>
+		<a href="/about" on:click={drawerClose} class={aStyle}>
+			<li class="{classesActive('/about')} {styleClass}">About</li>
+		</a>
+		<a href="/projects" on:click={drawerClose} class={aStyle}>
+			<li class="{classesActive('/projects')} {styleClass}">Projects</li>
+		</a>
+		<a href="/contacts" on:click={drawerClose} class={aStyle}>
+			<li class="{classesActive('/contacts')} {styleClass}">Contact Me!</li>
+		</a>
 	</ul>
 </nav>
