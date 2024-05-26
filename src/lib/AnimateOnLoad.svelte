6374a8c1674b3cf08be3ef8e1b338a2874ba4085
vighub <gvigan97@gmail.com>
2024-05-26 23:21:08 +0200

    added animate on load

---
+++ src/lib/AnimateOnLoad.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { fade } from 'svelte/transition';
+	import { navigating } from '$app/stores';
+	import { onMount } from 'svelte';
+
+	let animate = !$navigating;
+	let loaded = false;
+
+	onMount(() => (loaded = true));
+</script>
+
+{#if animate}
+	{#if loaded}
+		<div in:fade={{ delay: 500, duration: 500 }} class="contents">
+			<slot />
+		</div>
+	{/if}
+{:else}
+	<slot />
+{/if}
